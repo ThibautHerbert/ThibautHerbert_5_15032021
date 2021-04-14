df627caaa171f58ea465814c7b370748a66b80a4
Thibaut <herbert.thibaut@protonmail.com>
2021-04-14 14:30:43 -0400

    responsive carousel + refactorisation fonction affichage option vernis

--- js/product/product.js
+++ js/product/product.js
@@ -7,8 +7,14 @@ let varnishDisabled = document.querySelector('.varnishDisabled');
 let url = new URL(window.location.href); // permet de charger la page product via le lien href de la page home
 let id = url.searchParams.get("id"); // permet de cibler l'élément clé (identifiant : id)pour le chargement de l'URL
 
-//varnishDisabled.style.background = 'red';
-
+// fonction pour faire apparaitre les options de vernis disponible par produit (nombre d'option irrégulier selon les produits)
+function showVarnish (varnishs) {
+    let optionVarnish = [];
+    for (let i = 0 ; i < varnishs.length; i++) {
+        optionVarnish += `<option value="${i}">${(varnishs[i])}</option>`;  
+    }
+    return optionVarnish;
+}
 //appel de l'API via une fonction async await avec la méthode fetch avec un paramètre qui récupère la variable id
 const fetchFurniture = async(id) =>{
 	return await fetch(`http://localhost:3000/api/furniture/${id}`).then(res => res.json());
@@ -16,19 +22,7 @@ const fetchFurniture = async(id) =>{
 // affichage d'un seul produit par page via la récupération de l'id produit dont les informations sont contenus dans l'API 
 const showFurniture = async(id) => {
 	var furniture = await fetchFurniture(id);
-    // essai 
-    //il faut chercher dans l'api si vernis option 2 n'existe pas, alors on n'affiche pas 
-    //for (color in furniture.varnish)
     
-    if(furniture.varnish[2] === undefined) { //varnish.value ?
-        
-        varnishDisabled.setAttribute("disabled", "true");
-        varnishDisabled.style.background = 'red';
-        //varnishDisabled.setAttribute("disabled", "true");
-        //this.optionvalue.style.display = 'none';
-    }
-// fin essai boucle vernis
-
 // affichage des données produit en 2 blocs :
 	pictureProduct.innerHTML = `
 				<img class="card-img-top" src="${furniture.imageUrl}" />
@@ -50,40 +44,13 @@ const showFurniture = async(id) => {
                 </select>
                 <div>
                     <label for="varnishSelect">Choisir un vernis :</label>
-                    <select name="varnish" class="varnish" id="varnishSelect">
+                    <select name="varnish">
                         <option value="">Sélectionnez votre vernis</option>
-                        <option value="0">${furniture.varnish[0]}</option>
-                        <option value="1">${furniture.varnish[1]}</option>
-                        <option value="2" class="varnishDisabled">${furniture.varnish[2]}</option>     
+                        ${this.showVarnish(furniture.varnish)}
                     </select>
                 </div>
 				`
-    // ne pas montrer option value="2" s'il n'y a pas de 3ème option de personnalisation (vernis) pour le produit
-    /*if(furniture.varnish[2] === undefined) {
-        descriptionProduct.innerHTML = `
-			<h2 class="card-title text-warning">${furniture.name}</h5>
-            <p class="card-text">N° du produit : ${furniture._id}</p>
-			<p class="">${Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }, { maximumSignificantDigits: 3 }).format(furniture.price/100)}</p>
-            <label for=quantity>Quantité :</label>
-            <select name="quantity" class="quantityCount">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>   
-            </select>
-            <div>
-                <label for="varnishSelect">Choisir un vernis :</label>
-                <select name="varnish" class="varnish" id="varnishSelect">
-                    <option value="">Sélectionnez votre vernis</option>
-                    <option value="0">${furniture.varnish[0]}</option>
-                    <option value="1">${furniture.varnish[1]}</option> 
-                </select>
-            </div>
-			`
-        }*/
-}; // fin du showFurniture
-
+};
 // si la variable id existe et n'est pas indéfini alors la fonction showFurniture s'exécute, sinon message d'alerte
 if (id) {
     showFurniture(id);
@@ -96,7 +63,7 @@ clickForCart.addEventListener('click', () => {
     // opérateur conditionnel https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
     let cart = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : {}; 
     // conditional operator : si le localStorage cart existe alors le récupère, sinon crée un objet vide
-
+    // condition pour ajouter au panier
     if (cart[id]) {
         cart[id] += quantity; // panier contient un ou plusieurs produits : ajout d'un nouvel article +=
         alert("Vous avez ajouté un nouvel article dans votre panier !");
